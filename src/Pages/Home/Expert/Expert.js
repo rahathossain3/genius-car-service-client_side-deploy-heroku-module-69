a765afd236c65e24d2b0dab1b7bc350c17820be3
rahathossain3 <rahathossain210@gmail.com>
2022-04-17 19:57:28 +0600

    experts add

---
+++ src/Pages/Home/Expert/Expert.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import expert1 from '../../../images/experts/expert-1.jpg'
+
+
+const Expert = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Expert;
