f77ca83c1f8e764c8f962c48d81d7008eec5df17
rahathossain3 <rahathossain210@gmail.com>
2022-04-28 07:52:19 +0600

    own server data add

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -5,12 +5,12 @@ import './Service.css'
 
 const Service = ({ service }) => {
 
-    const { id, name, img, description, price } = service;
+    const { _id, name, img, description, price } = service;
 
     const navigate = useNavigate();
 
     const navigateToServiceDetail = id => {
-        navigate(`/service/${id}`);
+        navigate(`/service/${_id}`);
 
     }
 
@@ -21,7 +21,7 @@ const Service = ({ service }) => {
             <h2>This is Service: {name}</h2>
             <p>Price: {price}</p>
             <p><small>{description}</small></p>
-            <button onClick={() => navigateToServiceDetail(id)} className='btn btn-primary'>Book: {name}</button>
+            <button onClick={() => navigateToServiceDetail(_id)} className='btn btn-primary'>Book: {name}</button>
         </div>
     );
 };
