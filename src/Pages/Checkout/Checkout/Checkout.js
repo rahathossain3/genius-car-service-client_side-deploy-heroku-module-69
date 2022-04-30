402f2b3365c62f69eb1a677a8c8fa7fda678d9a4
rahathossain3 <rahathossain210@gmail.com>
2022-05-01 04:22:54 +0600

    checkout page checkout info create:

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useServiceDetail from '../../../hooks/useServiceDetail';
 
@@ -6,13 +6,43 @@ const Checkout = () => {
     const { serviceId } = useParams();
     const [service] = useServiceDetail(serviceId);
 
+
+    const [user, setUser] = useState({
+        name: 'akbar the ',
+        email: 'akbar@momo.taj',
+        address: 'Tajmohal road Md pur',
+        phone: "0171111111"
+    });
+
+    // for update user
+    const handleAddressChange = event => {
+        console.log(event.target.value);
+        const { address, ...rest } = user;
+        const newAddress = event.target.value;
+        const newUser = { address: newAddress, ...rest };
+
+        setUser(newUser);
+        console.log(newUser);
+    }
+
     return (
         <div className='w-50  mx-auto'>
             <h2>Pleases Order : {service.name}</h2>
 
             <form >
-                <input type="text" name='name' placeholder='name' required />
+                <input className='w-100 mb-2' type="text" value={user.name} name='name' placeholder='name' required />
+                <br />
+                <input className='w-100 mb-2' type="email" value={user.email} name='email' placeholder='email' required />
+                <br />
+                <input className='w-100 mb-2' type="text" value={service.name} name='service' placeholder='service' required />
+                <br />
+                <input className='w-100 mb-2' type="text" onChange={handleAddressChange} value={user.address} name='address' placeholder='address' required />
+                <br />
+                <input className='w-100 mb-2' type="text" value={user.phone} name='phone' placeholder='phone' required />
+                <br />
+                <input className='btn btn-primary' type="submit" value="Place Order" />
             </form>
+
 
         </div>
     );
