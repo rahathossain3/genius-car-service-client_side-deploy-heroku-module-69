da5b473f59ea25118b9faefb5e09106c6d49b01b
rahathossain3 <rahathossain210@gmail.com>
2022-05-01 17:38:53 +0600

    get user wise data show client side

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Order = () => {
+    const [user] = useAuthState(auth)
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+
+        const getOrders = async () => {
+            //user wish product
+            const email = user.email;
+
+            const url = `http://localhost:5000/order?email=${email}`;
+            const { data } = await axios.get(url);
+            setOrders(data);
+
+        }
+        getOrders();
+
+
+    }, [user])
+
+
     return (
         <div>
-            <h2>Your Orders</h2>
+            <h2>Your Orders: {orders.length} </h2>
         </div>
     );
 };
