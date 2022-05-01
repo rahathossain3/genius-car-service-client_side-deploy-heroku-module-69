dc577ef88b68f936132583eda1c5e66b63869e7c
rahathossain3 <rahathossain210@gmail.com>
2022-05-01 21:54:00 +0600

    fake or broken token problem handle

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
 import auth from '../../firebase.init';
 
 const Order = () => {
     const [user] = useAuthState(auth)
     const [orders, setOrders] = useState([]);
+
+    //for unauthorize user
+    const navigate = useNavigate();
 
     useEffect(() => {
 
@@ -14,13 +19,24 @@ const Order = () => {
             const email = user.email;
 
             const url = `http://localhost:5000/order?email=${email}`;
-            const { data } = await axios.get(url, {
-                headers: {
-                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
-                }
-            });
-            setOrders(data);
+            //when get fake token
+            try {
+                const { data } = await axios.get(url, {
+                    headers: {
+                        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                setOrders(data);
 
+            }
+            catch (error) {
+                console.log(error.message);
+                // fake token
+                if (error.response.status === 401 || error.response.status === 403) {
+                    signOut(auth);
+                    navigate('/login');
+                }
+            }
         }
         getOrders();
 
